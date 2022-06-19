8a49b6aa9a27ab33c7b85ce218e869ab8ec51002
NiranjanV12 <niranjanvaity@gmail.com>
2022-06-19 22:14:38 +0530

    alertmanager

--- kong-prom/curls.sh
+++ kong-prom/curls.sh
@@ -1,14 +1,14 @@
 while true;
 do
-  curl http://10.96.229.174:31392/microsvc-static-1/health
-  curl http://10.96.229.174:31392/microsvc-static-1/health
-  curl http://10.96.229.174:31392/microsvc-static-1/health
-  curl http://10.96.229.174:31392/microsvc-static-1/health
-  curl http://10.96.229.174:31392/microsvc-static-1/callOtherService
-  curl http://10.96.229.174:31392/microsvc-static-1/callOtherService
-  curl http://10.96.229.174:31392/microsvc-static-1/callOtherService
-  curl http://10.96.229.174:31392/microsvc-static-1/health
-  curl http://10.96.229.174:31392/microsvc-static-1/health
-  curl http://10.96.229.174:31392/microsvc-static-1/health
-  sleep 0.01
+  curl http://10.96.229.174:32048/microsvc-static-1/healtha
+  curl http://10.96.229.174:32048/microsvc-static-1/healtha
+  curl http://10.96.229.174:32048/microsvc-static-1/healtha
+  curl http://10.96.229.174:32048/microsvc-static-1/healtha
+  curl http://10.96.229.174:32048/microsvc-static-1/healtha
+  curl http://10.96.229.174:32048/microsvc-static-1/healtha
+  curl http://10.96.229.174:32048/microsvc-static-1/healtha
+  echo " "
+  echo " "
+  echo " "
+  sleep 5
 done
