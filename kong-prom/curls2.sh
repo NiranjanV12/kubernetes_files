3cd1128c9fc3514def8213281a8c5460458f3524
NiranjanV12 <niranjanvaity@gmail.com>
2022-06-20 21:40:33 +0530

    alertmanager config and separate ingress obj for each url

---
+++ kong-prom/curls2.sh
@@ -0,0 +1,14 @@
+while true;
+do
+  curl http://10.96.229.174:32048/microsvc-static-2/health
+  curl http://10.96.229.174:32048/microsvc-static-2/health
+  curl http://10.96.229.174:32048/microsvc-static-2/health
+  curl http://10.96.229.174:32048/microsvc-static-2/health
+  curl http://10.96.229.174:32048/microsvc-static-2/health
+  curl http://10.96.229.174:32048/microsvc-static-2/health
+  curl http://10.96.229.174:32048/microsvc-static-2/health
+  echo " "
+  echo " "
+  echo " "
+  sleep 5
+done
---
+++ kong-prom/curls3.sh
@@ -0,0 +1,14 @@
+while true;
+do
+  curl http://10.96.229.174:32048/microsvc-static-1/load
+  curl http://10.96.229.174:32048/microsvc-static-1/load
+  curl http://10.96.229.174:32048/microsvc-static-1/load
+  curl http://10.96.229.174:32048/microsvc-static-1/load
+  curl http://10.96.229.174:32048/microsvc-static-1/load
+  curl http://10.96.229.174:32048/microsvc-static-1/load
+  curl http://10.96.229.174:32048/microsvc-static-1/load
+  echo " "
+  echo " "
+  echo " "
+  sleep 5
+done
